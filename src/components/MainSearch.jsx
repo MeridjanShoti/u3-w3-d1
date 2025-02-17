56880604f72666ae9101e6131e9534c9d6154d19
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-17 16:15:23 +0100

    a

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,25 +1,25 @@
-import { useState } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import Job from "./Job";
+import { useDispatch, useSelector } from "react-redux";
 
 const MainSearch = () => {
-  const [query, setQuery] = useState("");
-  const [jobs, setJobs] = useState([]);
-
+  const jobs = useSelector((state) => state.jobs);
+  const query = useSelector((state) => state.query);
+  const dispatch = useDispatch();
   const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?search=";
 
-  const handleChange = e => {
-    setQuery(e.target.value);
+  const handleChange = (e) => {
+    dispatch({ type: "ADDQUERY", payload: e.target.value });
   };
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       const response = await fetch(baseEndpoint + query + "&limit=20");
       if (response.ok) {
         const { data } = await response.json();
-        setJobs(data);
+        dispatch({ type: "ADDJOBS", payload: data });
       } else {
         alert("Error fetching results");
       }
@@ -40,7 +40,7 @@ const MainSearch = () => {
           </Form>
         </Col>
         <Col xs={10} className="mx-auto mb-5">
-          {jobs.map(jobData => (
+          {jobs.map((jobData) => (
             <Job key={jobData._id} data={jobData} />
           ))}
         </Col>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,7 +5,9 @@ const initialState = {
 const mainReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADDJOBS":
-      return { ...state, jobs: [...state.jobs, action.payload] };
+      return { ...state, jobs: [...state.jobs, ...action.payload] };
+    case "ADDQUERY":
+      return { ...state, query: action.payload };
     default:
       return state;
   }
