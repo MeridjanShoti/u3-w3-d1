d02cd08d4a1486cdf42387d92ab2db0959c39e7e
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-17 18:55:48 +0100

    a

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -7,10 +7,8 @@ const Job = ({ data }) => {
   return (
     <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
       <Col xs={3}>
-        <Link to={`/${data.company_name}`}>
-          {data.company_name}{" "}
-          <span onClick={() => dispatch({ type: "ADD_FAVOURITE", payload: data.company_name })}>♡</span>
-        </Link>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+        <button onClick={() => dispatch({ type: "ADD_FAVOURITE", payload: data.company_name })}>♡</button>
       </Col>
       <Col xs={9}>
         <a href={data.url} target="_blank" rel="noreferrer">
